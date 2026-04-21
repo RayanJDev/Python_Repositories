a60c672f3465880c43846f586ccd9343f8562a5e
RayanJDev <reys.rodrigues.96@hotmail.com>
2026-04-20 21:52:59 -0300

    Refactor file searching script with new functions
    Added functions to read a file, get user input for a letter, and search for names starting with that letter. Improved structure and functionality of the script.

--- py/manipulacao_de_arquivos/procurando_primeira_letra.py
+++ py/manipulacao_de_arquivos/procurando_primeira_letra.py
@@ -5,8 +5,39 @@ a: adiciona/anexa ao final do arquivo
 r+: leitura e escrita
 """
 import re
-from collections import  Counter
 
-def procurar_letra(arquivo):
-    with open(arquivo,'r',enconding='utf-8'):
-        
+arquivo = "arquivo.txt"
+
+def ler_arquivo(arqu):
+    with open(arqu,'r',encoding='utf-8') as file:
+        return file.read() 
+
+def digitar_letra():
+    letra = input("Digite a letra que procura: ").strip()
+
+    if len(letra) != 1 or not letra.isalpha():
+        print("Digite apenas uma letra!")
+        return None
+    return letra
+
+def buscar_primeira_letra(texto, letra):
+    padrao = rf"\b{re.escape(letra)}\w*"
+    return re.findall(padrao,texto,re.IGNORECASE)
+
+def main():
+    texto = ler_arquivo(arquivo)
+    letra = digitar_letra()
+
+    if letra is None:
+        return
+
+    nomes_encontrados = buscar_primeira_letra(texto, letra)
+
+    print(f'\nNomes encontrados com a letra {letra}: ')
+    for nome in nomes_encontrados:
+        print(nome)
+
+    print(f'Quantidade encontrada: {len(nomes_encontrados)}')
+
+if __name__ == "__main__":
+    main()
